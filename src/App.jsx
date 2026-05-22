eb5a8e5d43305e869ac936586394cbcd44596c72
tayyabamuzammal <tayyabamuzammal24@gmail.com>
2026-05-22 08:03:24 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 function App() {
-  return <h1>Hello</h1>
+  return (
+    <div>
+      <h1 style={{background:"red", color:"white", padding:"20px"}}>
+        My Website Working ✅
+      </h1>
+    </div>
+  )
 }
 
 export default App
